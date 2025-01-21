51361a0c40d6d9ec1fe49bbe218c102cfd5de3bb
EJS00102 <52255287+EJS00102@users.noreply.github.com>
2025-01-21 12:07:30 +0200

    Updated Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,27 @@
 import { reactRouter } from "@react-router/dev/vite";
+import autoprefixer from "autoprefixer";
 import path from "path";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const domainUrl = new URL(process.env.DOMAIN_URL || "http://localhost:3000");
-
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
   plugins: [reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
     },
   },
-  server: {
-    host: domainUrl.hostname,
-    port: parseInt(domainUrl.port) || 3000,
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
   },
-});
+}));
