579a0692905fd0a6a603ebb1e76b0dd6dafb4df7
EJS00102 <52255287+EJS00102@users.noreply.github.com>
2025-01-24 14:51:05 +0200

    Working now

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,39 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
+import esbuild from "esbuild";
 import path from "path";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const domainUrl = new URL(process.env.DOMAIN_URL || "http://localhost:3000");
-
-export default defineConfig({
+export default defineConfig(({ isSsrBuild }) => ({
+  buildEnd: async () => {
+    await esbuild
+      .build({
+        alias: { "~": "./app" },
+        outfile: "build/server/index.js",
+        entryPoints: ["server/index.ts"],
+        external: ["./build/server/*"],
+        platform: "node",
+        format: "esm",
+        packages: "external",
+        bundle: true,
+        logLevel: "info",
+      })
+      .catch((error: unknown) => {
+        console.error("Error building server:", error);
+        process.exit(1);
+      });
+  },
   plugins: [reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./app"),
     },
   },
-  server: {
-    host: domainUrl.hostname,
-    port: parseInt(domainUrl.port) || 3000,
-  },
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-});
+}));
