fb0a477523d775e67e4250cfab57eddcef21f60c
EJS00102 <52255287+EJS00102@users.noreply.github.com>
2025-01-28 10:35:26 +0200

    Added Github link to index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -29,10 +29,23 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div className="flex min-h-full flex-1 flex-col sm:px-6 lg:px-8 pt-6 space-y-6 px-3">
-      <div className="sm:mx-auto sm:w-full">
+      <div className="sm:mx-auto sm:w-full flex justify-between items-center">
         <h1 className="text-4xl">
           Welcome to the <strong>{siteData.name}!</strong>
         </h1>
+        <a
+          href="https://github.com/ejscheepers/strato-stack"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-500 hover:text-gray-900 flex items-center gap-2"
+        >
+          View on Github
+          <img
+            height="32"
+            width="32"
+            src="https://cdn.simpleicons.org/github"
+          />
+        </a>
       </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <div className="bg-white shadow-lg rounded-lg p-6">
@@ -91,9 +104,7 @@ export default function Index() {
           </p>
         </div>
         <div className="bg-white shadow-lg rounded-lg p-6">
-          <h2 className="text-xl font-semibold">
-            Deploy Anywhere using Docker
-          </h2>
+          <h2 className="text-xl font-semibold">Docker Ready</h2>
           <p>
             The {siteData.name} is ready to be deployed anywhere using Docker.
             No vendor lock-in.
