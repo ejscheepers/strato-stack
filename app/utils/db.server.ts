98d0bfa8ea125405647a55b94ab6c94b2df8ea70
EJS00102 <52255287+EJS00102@users.noreply.github.com>
2025-06-06 14:56:08 +0200

    Small fixes

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
-import { Pool } from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
 
 // Regular connection through PgBouncer for application operations
 export const db = drizzle({
